4fc193308e1fb507cae41128057c7835cf5b3b4c
githubsag <sohrabag@kth.se>
2021-11-17 21:35:33 +0100

    first commit

---
+++ confsvr/src/main/java/com/thoughtmechanix/confsvr/ConfigServerApplication.java
@@ -0,0 +1,18 @@
+package com.thoughtmechanix.confsvr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableConfigServer
+public class ConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigServerApplication.class, args);
+	}
+
+}
